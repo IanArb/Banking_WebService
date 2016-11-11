0cb225b454dbe5c868cd3363660135c8c2975e75
Conor Keenan <Conork23@gmail.com>
2016-11-11 16:10:48 +0000

    fixed ordering of email and phone in service constructor

--- src/main/java/com/mycompany/banking_webservice/CustomerService.java
+++ src/main/java/com/mycompany/banking_webservice/CustomerService.java
@@ -18,10 +18,10 @@ public class CustomerService {
     
     public CustomerService(){
         if(people.isEmpty()){
-            people.add(new Person(1,"Conor","Dublin","018657477","Conor@test.com"));
-            people.add(new Person(2,"Jessie","Meath","018633477","Jessie@test.com")); 
-            people.add(new Person(3,"Shane","Cork","018612747","Shane@test.com"));
-            people.add(new Person(4,"Ben","Dublin","018651377","Ben@test.com"));
+            people.add(new Person(1,"Conor","Dublin","Conor@test.com", "018657477"));
+            people.add(new Person(2,"Jessie","Meath","Jessie@test.com","018633477")); 
+            people.add(new Person(3,"Shane","Cork","Shane@test.com","018612747"));
+            people.add(new Person(4,"Ben","Dublin","Ben@test.com","018651377"));
             
         }
     }
