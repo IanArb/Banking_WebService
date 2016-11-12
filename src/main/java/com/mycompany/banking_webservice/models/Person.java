6d47e0ef4bbb069bbe702687c6744049b6f4ed61
Ian Arbuckle <ian.arbuckle18@gmail.com>
2016-11-12 13:22:20 +0000

    Test database

--- src/main/java/com/mycompany/banking_webservice/models/Person.java
+++ src/main/java/com/mycompany/banking_webservice/models/Person.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.banking_webservice.objects;
+package com.mycompany.banking_webservice.models;
 
 /**
  *
